16ed8b347d364ce60bf37f58342488ad0328ac04
Katy Chan <cchan@my.bcit.ca>
2023-04-03 16:59:33 -0700

    make cancel button clickable for removing textarea/save/cancel button

--- scripts/script.js
+++ scripts/script.js
@@ -9,9 +9,23 @@ function turnDarkTheme () {
     backGroundColor.textContent = "Light Theme";
   } else if (backGroundColor.textContent === "Light Theme") {
     backGroundColor.textContent = "Dark Theme";
-  }
+  };
   
-}
+};
 
 
 backGroundColor.addEventListener("click", turnDarkTheme);
+
+
+const cancelButton = document.querySelector(".cancelb")
+const saveButton = document.querySelector(".saveb")
+const textArea = document.querySelector("textarea")
+
+
+function hideTextAreaandButton () {
+    textArea.classList.add("hidden");
+    saveButton.classList.add("hidden");
+    cancelButton.classList.add("hidden");
+};
+
+cancelButton.addEventListener("click", hideTextAreaandButton);
